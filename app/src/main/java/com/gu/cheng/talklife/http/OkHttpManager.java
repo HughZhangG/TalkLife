4cb63f8de4a888247c200eb9cb3c903188cfa39c
Administrator <411988153@qq.com>
2016-06-01 23:52:55 +0800

    zhangguangcheng

---
+++ app/src/main/java/com/gu/cheng/talklife/http/OkHttpManager.java
@@ -0,0 +1,11 @@
+package com.gu.cheng.talklife.http;
+
+
+/**
+ * Created by Guangcheng.Zhang on 2016/6/1 23:13.
+ * Email:zhang411988153@163.com
+ */
+
+public  class OkHttpManager {
+
+}
---
+++ app/src/main/java/com/gu/cheng/talklife/util/Contant.java
@@ -0,0 +1,10 @@
+package com.gu.cheng.talklife.util;
+
+/**
+ * Created by Guangcheng.Zhang on 2016/6/1 23:09.
+ * Email:zhang411988153@163.com
+ */
+
+public class Contant {
+    public static final String BATH_URL = "http://op.juhe.cn/onebox/news/query?key=1832cd0c87752a36b381b3221c4f090f?dtype=json";
+}
